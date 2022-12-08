d14a7fdb48379c8efe659bca8202ce1e0bbfcbd6
Marek Kubica <marek@tarides.com>
2022-12-08 20:02:27 +0100

    Remove `List.compare`

--- stdext/list.ml
+++ stdext/list.ml
@@ -9,18 +9,6 @@ let max_exn ~compare l =
           | Gt | Eq -> acc
           | Lt -> elm)
 
-let compare ~compare l l' =
-  let rec aux l l' =
-    match (l, l') with
-    | hd :: tl, hd' :: tl' ->
-        let c = compare hd hd' in
-        if c = 0 then aux tl tl' else c
-    | [], _ :: _ -> Ordering.to_int Lt
-    | _ :: _, [] -> Ordering.to_int Gt
-    | [], [] -> 0
-  in
-  aux l l'
-
 let unzip l =
   let xs, ys =
     fold_left ~f:(fun (xs, ys) (x, y) -> (x :: xs, y :: ys)) ~init:([], []) l
--- stdext/list.mli
+++ stdext/list.mli
@@ -6,5 +6,4 @@ val max_exn : compare:('a -> 'a -> int) -> 'a list -> 'a
 (** Returns the greatest element in the list according to [compare].
     Raises Invalig_argument on empty lists *)
 
-val compare : compare:('a -> 'a -> int) -> 'a list -> 'a list -> int
 val unzip : ('a * 'b) list -> 'a list * 'b list
